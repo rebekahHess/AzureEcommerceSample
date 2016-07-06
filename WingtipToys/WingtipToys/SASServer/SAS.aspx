fc23c3b98c79a1f325c5d5b1f4138552437f02a7
Jon Seaman <jonmseaman@live.com>
2016-07-06 11:07:00 -0700

    Added simple SAS server to web page

---
+++ WingtipToys/WingtipToys/SASServer/SAS.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SAS.aspx.cs" Inherits="WingtipToys.SASServer.SAS" %>
---
+++ WingtipToys/WingtipToys/SASServer/SAS.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.ServiceBus;
+
+namespace WingtipToys.SASServer
+{
+    public partial class SAS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(GetNewSAS());
+        }
+
+        public static string GetNewSAS()
+        {
+            // Test stuff.
+            string keyName = "Managed";
+            string key = "oRt/agurnQYUDRsvm0tOKOEi2e5nXr0MnTrUxP8PdTw=";
+            string @namespace = "ServiceBusIntern2016";
+            TimeSpan ttl = TimeSpan.FromHours(1);
+            string sas = SharedAccessSignatureTokenProvider.GetSharedAccessSignature(keyName, key, @namespace, ttl);
+
+            return sas;
+        }
+    }
+}
